7e9efe7a0572541cf3b2b196916a4c1ede32b4ef
Lucas Jimenez-Suselo <lucasaj2@uci.edu>
2025-02-23 12:35:59 -0800

    Tokenize soup object

--- tokenize_and_stem.py
+++ tokenize_and_stem.py
@@ -1,15 +1,17 @@
 from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
+from bs4 import BeautifulSoup
 
 stemmer = PorterStemmer()
 # Ex: "The cats were chasing mice quickly" -> "The cat were chase mice quickli"
 
 """ 
-    Given a string, parse through and tokenize each alphanumeric word.
+    Given a Soup object, parse through and tokenize each alphanumeric word.
     Then, using Porter Stemmer Algorithm, stem each token (get rid of suffix) and
     keep its root form.
+    Returns a list of the stemmed tokens.
 """
-def tokenize_and_stem(text: str) -> list :
-    tokens = word_tokenize(text)
-    stems = [ stemmer.stem(token) for token in tokens if token.isalnum() ]
+def tokenize_and_stem(soup: BeautifulSoup) -> list:
+    tokens = word_tokenize(soup.get_text().lower())
+    stems = [stemmer.stem(token) for token in tokens if token.isalnum()]
     return stems
