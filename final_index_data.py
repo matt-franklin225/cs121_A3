384d93d98131d06bbdef9b983adf62a99a65bb31
Matt Franklin <mjfrankl@uci.edu>
2025-02-23 22:53:42 -0800

    M1 Complete

--- final_index_data.py
+++ final_index_data.py
@@ -1,5 +1,6 @@
 import json
 
+# Simply used to calculate number of tokens
 if __name__ == '__main__':
     with open('merged_index.json', 'r') as index:
         data = json.load(index)
--- indexer.py
+++ indexer.py
@@ -4,7 +4,8 @@ from bs4 import BeautifulSoup as BS
 from collections import defaultdict
 import tokenize_and_stem as tokenizer
 
-def calculate_tf(tokens: list) -> dict:
+
+def calculate_frequencies(tokens: list) -> dict:
     tf = defaultdict(int)
     for token in tokens:
         tf[token] += 1
@@ -15,48 +16,40 @@ def build_partial_index(file_paths: list, index_file_name: str) -> None:
     inverted_index = defaultdict(list)
     tokens_unique = list(set())
 
-    for file_path in file_paths: # doc[0] is the ID, doc[1] is the file path
+    # Iterate through the files, tokenizing and getting frequencies for each
+    for file_path in file_paths: # doc[0] is the document ID, doc[1] is the file path
         with open(file_path[1], 'r') as file:
             print(file_path[0]) # REMOVE THIS LATER
             content = json.dumps(json.load(file))
             soup = BS(content, "html.parser")
             tokens = tokenizer.tokenize_and_stem(soup)
-            tf = calculate_tf(tokens)
+            tf = calculate_frequencies(tokens)
             tokens_unique = list(set(tokens))
             for token in tokens_unique:
                 inverted_index[token].append((file_path[0], tf[token]))
 
+    # Write inverted index to file
     with open(index_file_name, 'w') as file:
         json.dump(inverted_index, file, indent=4)
     inverted_index.clear()
 
 
-# def build_inverted_index(root_folder):
-#     documents = []
-#     current_doc_id = 0
-#     current_p_index = 0
-#     DOCUS_PER_PARTIAL_INDEX = 1000
-#     for dir, subdirs, files in os.walk(root_folder):
-#         for file in files:
-#             current_doc_id += 1
-#             documents.append([current_doc_id, os.path.join(dir, file)])
-#             if current_doc_id % DOCUS_PER_PARTIAL_INDEX == 0:
-#                 build_partial_index(documents, f"partial_index/p-index{current_p_index}.json")
-#                 current_p_index += 1
-#                 documents.clear()
-
 def build_inverted_index(root_folder):
     documents = []
     current_doc_id = 0
-    starting_doc = 55001
+    # Can manually adjust starting point, this is done in case
+    # we suffer a crash or other issue midway through a run
+    starting_doc = 0
     current_p_index = starting_doc // 1000
     DOCUS_PER_PARTIAL_INDEX = 1000
     for dir, subdirs, files in os.walk(root_folder):
         for file in files:
             current_doc_id += 1
+            # Skip over documents before the starting point (if necessary)
             if current_doc_id < starting_doc:
                 continue
             documents.append([current_doc_id, os.path.join(dir, file)])
+            # Every 1k documents, build out a new partial index
             if current_doc_id % DOCUS_PER_PARTIAL_INDEX == 0:
                 build_partial_index(documents, f"partial_index/p-index{current_p_index}.json")
                 current_p_index += 1
@@ -67,5 +60,4 @@ def build_inverted_index(root_folder):
 
 if __name__ == "__main__":
     root_folder = "DEV"
-    # inverted_index = build_inverted_index(root_folder)
     build_inverted_index(root_folder)
