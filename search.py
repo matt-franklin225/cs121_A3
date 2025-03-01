7552c693df85f1dcfae5c43e101ea8309acbf06d
Matt Franklin <mjfrankl@uci.edu>
2025-02-28 16:03:18 -0800

    Initial Helper Functions Created

--- search.py
+++ search.py
@@ -27,15 +27,44 @@ Evaluation criteria
 • Did your report show up on time?
 • Are the reported URLs plausible?
 """
+
+"""
+TODO (for M2):
+Write helper functions for document at a time retrieval function
+- get_current_document
+- go_to_document
+- get_score
+- go_to_next_document
+Update inverted index to work better
+- Add a dictionary that maps doc_ids to urls
+- (if we have time) Update to add tf/idf scores for more precise retrieval
+"""
+
 import heapq
 
-def document_at_a_time_retrieval(query):
+# Returns the current document of a given list
+def get_current_document(inv_list: list) -> int:
+    pass
+
+# Returns next document that contains the inv_list's term, starting at the given doc (returns doc if doc contains the term)
+def go_to_document(inv_list: list, doc: int) -> int:
+    pass
+
+# Returns the document's score for a given query
+def get_score(query: list, doc: int) -> int:
+    pass
+
+# Sends the given list to the next document
+def go_to_next_document(inv_list: list) -> None:
+    pass
+
+def document_at_a_time_retrieval(query: str) -> list:
     inverted_lists = [] # Stores an inverted list for each term in the query
     results = []
     for term in query:
-        # term_indexes.append(inverted_list(term, inverted_index))
         # Final implementation: add the inverted list 
         # for each term in the query into this array
+        # term_indexes.append(inverted_list(term, inverted_index))
         inverted_lists.append({}) # Placeholder
     document = -1
 
@@ -44,13 +73,13 @@ def document_at_a_time_retrieval(query):
     # while inverted_lists:
     #     doc_score = 0
     #     for list in inverted_lists:
-    #         if get_current_document(list) > document: # TODO: write get_next(list)
+    #         if get_current_document(list) > document: # TODO: write get_current_document
     #             document = get_current_document(list)
     #     for list in inverted_lists:
     #         list.go_to_document(document) # TODO: Write go_to_document, goes to next document that >= {document}
     #         if get_current_document(list) == document:
     #             doc_score += get_score(query, document) # TODO: Write get_score
-    #             list.go_to_next_document() # TODO: Set list to the next document
+    #             list.go_to_next_document() # TODO: write go_to_next_document
     #         else:
     #             document = -1
     #             break
