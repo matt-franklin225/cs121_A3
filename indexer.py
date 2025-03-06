5e5bd6606a5874d85f7d5536c064423785588a5c
Matt Franklin <mjfrankl@uci.edu>
2025-03-05 16:00:30 -0800

    Indexer Updated for Txt File Strategy

--- indexer.py
+++ indexer.py
@@ -14,36 +14,39 @@ def calculate_frequencies(tokens: list) -> dict:
         tf[token] += 1
     return tf
 
-
 def build_partial_index(file_paths: list, index_file_name: str) -> None:
     inverted_index = defaultdict(list)
 
     # Iterate through the files, tokenizing and getting frequencies for each
-    for file_path in file_paths: # doc[0] is the document ID, doc[1] is the file path
+    for file_path in file_paths:
         with open(file_path[1], 'r') as file:
-            print(file_path[0]) # REMOVE THIS LATER
+            print(file_path[0])
             content = json.load(file)
             doc_ids.add(file_path[0], content["url"])
 
             soup = BS(content["content"], "html.parser")
             tokens = tokenizer.tokenize_and_stem(soup)
             tf = calculate_frequencies(tokens)
-            tokens_unique = list(set(tokens))
+            tokens_unique = set(tokens)
+            
             for token in tokens_unique:
                 posting = Posting(file_path[0], content["url"])
                 posting.set_tf(tf[token])
                 posting.calculate_tf_score(len(tokens), tf[token])
                 inverted_index[token].append(posting)
 
-    # Write inverted index to file
+    # Write partial index to file
     with open(index_file_name, 'w') as file:
-        json.dump(inverted_index, file, indent=4, cls=PostingEncoder)
+        for term, postings in inverted_index.items():
+            postings_str = " | ".join(f"{p.id},{p.url},{p.tf_score}" for p in postings)
+            file.write(f"{term}: {postings_str}\n")
+
     inverted_index.clear()
 
 
-def build_inverted_index(root_folder):
+def build_inverted_index(root_folder: str, target_folder: str):
     documents = []
-    current_doc_id = 0
+    current_doc_id = -1
     # Can manually adjust starting point, this is done in case
     # we suffer a crash or other issue midway through a run
     starting_doc = 0
@@ -59,17 +62,16 @@ def build_inverted_index(root_folder):
                 continue
             documents.append([current_doc_id, os.path.join(dir, file)])
             # Every 1k documents, build out a new partial index
-            if current_doc_id % DOCUS_PER_PARTIAL_INDEX == 0:
-                build_partial_index(documents, f"partial_index/p-index{current_p_index}.json")
+            if (current_doc_id+1) % DOCUS_PER_PARTIAL_INDEX == 0:
+                build_partial_index(documents, f"{target_folder}/p-index{current_p_index}.txt")
                 current_p_index += 1
                 documents.clear()
-    build_partial_index(documents, f"partial_index/p-index{current_p_index}.json")
+    build_partial_index(documents, f"{target_folder}/p-index{current_p_index}.txt")
     current_p_index += 1
     documents.clear()
 
-
-
 if __name__ == "__main__":
     root_folder = "DEV"
-    build_inverted_index(root_folder)
+    target_folder = "partial_index_test"
+    build_inverted_index(root_folder, target_folder)
     doc_ids.write_to_file("url_ids.json")
